438f6aeb2ef15cc915546f7f8af93c8258c776f6
arturo <arturodelosangeles@hotmail.com>
2018-08-12 22:05:17 -0500

    add nide clas

---
+++ ProgrammingInterview/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingInterview
+{
+    class Node
+    {
+        public int value;
+
+        public Node(int value)
+        {
+
+            this.value = value; 
+
+        }
+
+        public override string ToString()
+        {
+            return ""+this.value;
+        }
+    }
+}
---
+++ ProgrammingInterview/Programs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingInterview
+{
+    class Programs
+    {
+
+        public static void testNode() {
+
+            Node n1 = new Node(1);
+            Node n2 = new Node(1);
+
+            n1.
+
+        }
+
+
+
+    }
+}
